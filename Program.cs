4d99a032fb3da203a77c2d09b8bbbf76b7a74fea
Vinit Suryarao <suryarao.vinit@hotmail.com>
2020-11-27 12:53:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{   
+    // Using Sealed Class
+    public sealed class A
+    {
+        int a = 5;
+    }
+
+    public class B : A
+    {
+        int b = 6;
+    }
+
+    // Using Private Class
+    public private class C
+    {
+        int a = 5;
+    }
+
+    public class D : C
+    {
+        int b = 6;
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
